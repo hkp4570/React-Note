9c6c6fefbf688b1167555f17b58fc2a1f7e7ec02
hkp <sxhkp163@163.com>
2019-11-25 18:17:35 +0800

    paging component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,10 @@
 // 你可能需要针对不同路由输出不同的全局 layout，umi 不支持这样的配置，
 // 但你仍可以在 layouts/index.js 对 location.path 做区分，渲染不同的 layout
-
+import React from 'react';
+import { Layout } from 'antd';
 import styles from './index.css';
 
+const { Header, Footer, Content } = Layout;
 function BasicLayout(props) {
   if(props.location.pathname === '/login'){
     return (
@@ -12,9 +14,15 @@ function BasicLayout(props) {
       </div>
     )
   }
+  {/*<h1 className={styles.title}>Yay! Welcome to react components</h1>*/}
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to react components</h1>
+      <Layout>
+        <Header>header</Header>
+        <Content className={styles.main}>content</Content>
+        <Footer>footer</Footer>
+      </Layout>
+
       {props.children}
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
-import styles from './index.css';
-import Paging from '@/component/paging/index';
 
-export default class  extends Component {
+import React, { Component } from 'react';
+
+export default class extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      current:5
-    };
+    this.state = {};
   };
-  pageChange(e){
-    this.setState({
-      current:e
-    });
-  }
+
+  componentDidMount() {
+
+  };
+
   render() {
     return (
-      <div className={styles.normal}>
-        <Paging
-          total={100}
-          current={this.state.current}
-          limit={5}
-          panelNumber={5}
-          onPageChange={(e) => this.pageChange(e)}
-        />
+      <div>
+首页
       </div>
     );
   }
