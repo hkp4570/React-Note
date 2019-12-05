ac095ea5f9916afaf512cc8fbb349093c1fc9436
hkp <sxhkp163@163.com>
2019-12-05 09:28:06 +0800

    umi config

--- .umirc.js
+++ .umirc.js
@@ -3,6 +3,11 @@
 
 export default {
   treeShaking: true,
+  // outputPath:"./out",      //使用umi build后，打包的目录名称，默认./dist
+  // history:"hash",          //history对象模式（默认是browser）
+  // base:"/abc",             //相当于之前BrowserRouter中的basename
+  // publicPath:"./publick",  //指定静态资源所在的目录
+  // exportStatic:true,          //开启该配置后，会打包成多个静态页面，每个页面对应一个路由，开启多静态页面应用的前提条件是：没有动态路由
   // routes: [
   //   {
   //     path: '/',
