c88d0897a7e57d50f1139b1757c9410bb3909094
hkp <sxhkp163@163.com>
2020-07-15 23:38:40 +0800

    React工具

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.less';
+import App from './app/app';
+export default () => {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
