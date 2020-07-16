efe5b95850e78b41dfb5af16f137efe5836a89e7
hkp <sxhkp163@163.com>
2020-07-16 17:17:46 +0800

    umi3

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# react_umi
-comp
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
--- src/pages/demo/renderProps.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RenderProps from '@/components/RenderProps/index3';
-export default (props) => {
-  return (
-    <div>
-      <RenderProps />
-    </div>
-  );
-}
