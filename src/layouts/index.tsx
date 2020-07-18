98b120e1f4d5c3e90406e34a67bf797368e39559
hkp <sxhkp163@163.com>
2020-07-18 14:10:11 +0800

    重新创建

---
+++ src/layouts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index: React.FC = (props: any) => {
+  return (
+    <div>
+      <h1>umi3</h1>
+      {props.children}
+    </div>
+  );
+};
+export default Index;
