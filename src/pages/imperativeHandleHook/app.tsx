2911aec8c21e5c7ea933e6a5cd44268659c3cac8
hkp <sxhkp163@163.com>
2020-08-20 15:46:27 +0800

    获取类的实例

--- src/pages/imperativeHandleHook/app.tsx
+++ src/pages/imperativeHandleHook/app.tsx
@@ -13,45 +13,46 @@ import React, { useRef, useImperativeHandle } from 'react';
 // }
 // const TestRef = React.forwardRef(Test);
 
-// class Test extends React.Component<any, any>{
-//   method(){
-//     console.log('test method');
-//   }
-//   render() {
-//     return <div>
-//       Test
-//     </div>;
-//   }
-// }
-
-function Test(props, ref) {
-  useImperativeHandle(
-    ref,
-    () => {
-      //如果不给依赖项，则每次运行函数组件都会调用该方法
-      //如果使用了依赖项，则第一次调用后，会进行缓存，只有依赖项发生变化时才会重新调用函数
-      //相当于给 ref.current = 1
-      return {
-        method() {
-          console.log('Test Component Called');
-        },
-      };
-    },
-    [],
-  );
-  return <h1>Test Component</h1>;
+class Test extends React.Component<any, any>{
+  method(){
+    console.log('test method');
+  }
+  render() {
+    return <div>
+      Test
+    </div>;
+  }
 }
+
+// function Test(props, ref) {
+//   useImperativeHandle(
+//     ref,
+//     () => {
+//       //如果不给依赖项，则每次运行函数组件都会调用该方法
+//       //如果使用了依赖项，则第一次调用后，会进行缓存，只有依赖项发生变化时才会重新调用函数
+//       //相当于给 ref.current = 1
+//       return {
+//         method() {
+//           console.log('Test Component Called');
+//         },
+//       };
+//     },
+//     [],
+//   );
+//   return <h1>Test Component</h1>;
+// }
 const TestRef = React.forwardRef(Test);
 
-/**
- * 获取子组件中的方法
- * @param props
- * @constructor
- */
+// /**
+//  * 获取子组件中的方法
+//  * @param props
+//  * @constructor
+//  */
 function App(props) {
   const ref = useRef();
   return (
     <div>
+       {/* <Test ref={ref} /> */}
       <TestRef ref={ref} />
       <button
         onClick={() => {
