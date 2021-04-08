54b1e890155a1cdb013812886a64421dc6d7ad5f
hkp <sxhkp163@163.com>
2021-04-08 19:05:02 +0800

    匹配404页面

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './routers/preventingTransition';
+import App from './routers/noMatch';
 
 ReactDOM.render(<App />,document.getElementById('root'));
---
+++ src/routers/noMatch.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function noMatch(props) {
+    return (
+        <Router>
+            <ul>
+                <li>
+                    <Link to='/'>home</Link>
+                </li>
+                <li>
+                    <Link to='/will_redirect'>will redirect</Link>
+                </li>
+                <li>
+                    <Link to='/old_redirect'>old redirect</Link>
+                </li>
+            </ul>
+
+            <Switch>
+                <Route exact path='/'>
+                    <Home></Home>
+                </Route>
+                <Route exact path='/will_redirect'>
+                    <WillRedirect />   
+                </Route>
+                <Route exact path='/old_redirect'>
+                    <Redirect to='/will_redirect'/>
+                </Route>
+                <Route path='*'>
+                    <NoMatch />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+function Home(){
+    return (
+        <h1>home</h1>
+    )
+}
+function WillRedirect(){
+    return (
+        <h1>WillRedirect</h1>
+    )
+}
+function NoMatch(){
+    return (
+        <h1>404</h1>
+    )
+}
+
+export default noMatch;
