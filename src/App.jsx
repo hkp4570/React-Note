38b4c75395c3af4028c5da201d8028fcf9eefa63
hkp <sxhkp163@163.com>
2021-09-14 14:22:01 +0800

    portals使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App(props) {
+function ChildA(){
+  return ReactDOM.createPortal(<div>
+    <h1>childA</h1>
+    <ChildB />
+  </div>,document.querySelector('.modal'))
+}
+
+function ChildB(){
+  return <h1>childB</h1>
+}
+
+function App() {
+  
+  const handleClick = e => {
+    console.log('可以冒泡', e.target);
+  }
   return (
-    <div>
-      APP
+    <div onClick={e => handleClick(e)}>
+      <h1>APP</h1>
+      <ChildA />
     </div>
   );
 }
